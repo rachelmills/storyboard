342553d23ac729a14a735a63af9c2337190366c1
rachelmills <rachel.mills@yahoo.co.uk>
2013-10-05 13:08:21 +1000

    Add weather to app

--- storyboard/ViewController.h
+++ storyboard/ViewController.h
@@ -10,4 +10,11 @@
 
 @interface ViewController : UIViewController
 
+#define DarkSkyURL @"https://api.forecast.io/forecast/"
+#define API @"84f8f82d46fd524ccf2a24a5c44c8e97"
+#define LAT @"/42.7243"
+#define LON @",-73.6927"
+#define UNITS @"?units=si"
+#define fullURL  (DarkSkyURL API LAT LON UNITS)
+
 @end
